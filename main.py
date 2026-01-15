cc0714465687c3d22e3742df65d18853e06ed084
Tod1805 <theodore.desmet18@gmail.com>
2026-01-15 14:21:14 +0100

    Refactor main.py for improved game flow and UI
    Updated the game interface and event handling for the chess game.

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
 import pygame
-from pygame.locals import *
+# from pygame.locals import *
 pygame.init()
 
-pygame.display.set_caption("Jeu d'échecs") # Créer la fenêtre et lui donner un nom
+pygame.display.set_caption("Jeu d'échecs")
 
 encours = True
-background = pygame.image.load("fondecran.jpg")
+menu = pygame.image.load("ecrannoir.png")
 echiquier = pygame.image.load("echiquier.jpg")
-screen = pygame.display.set_mode((1280,720))
-screen.blit(background, (0, 0))
+game_over = pygame.image.load("ecranfingameover.jpg")
+screen = pygame.display.set_mode((900,900))
+screen.blit(menu, (0, 0))
+
+police = pygame.font.SysFont("Arial", 40, bold=True)
+texte_accueil1 = police.render("Bienvenue sur notre jeu d'échecs", True, (255, 255, 255))
+texte_accueil2 = police.render("Appuyez sur ESPACE pour jouer", True, (255, 255, 255))
+texte_revenir_accueil = police.render("Appuyez sur ENTREE pour revenir à l'accueil", True, (255, 255, 255))
+texte_quitter1 = police.render("Appuyer sur Q pour quitter le jeu", True, (255, 255, 255))
+texte_quitter2 = police.render("Appuyer sur S pour vraiment le jeu", True, (255, 255, 255))
+screen.blit(texte_accueil1, (150, 40))
+screen.blit(texte_accueil2, (150, 400))
+
 
 while encours :
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             encours = False
-        if event.type == KEYDOWN:
-            if event.key == K_SPACE:
-                screen.blit(echiquier, (0, 0))
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                screen.blit(menu, (0, 0))
+                screen.blit(echiquier, ((140, 150)))
+            if event.key == pygame.K_q:
+                screen.blit(menu, (0, 0))
+                screen.blit(game_over, (220, 200))
+                screen.blit(texte_revenir_accueil, (20, 20))
+            if event.key == pygame.K_RETURN:
+                screen.blit(menu, (0, 0))
+                screen.blit(texte_accueil1, (150, 40))
+                screen.blit(texte_accueil2, (150, 400))
+            if event.key == pygame.K_s:
+                encours = False
+
 pygame.quit()
