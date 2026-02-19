41e5cd8ff9c224d85723beb847f266d3cc230615
Tod1805 <theodore.desmet18@gmail.com>
2026-02-19 14:59:33 +0100

    Add soundbackground_tod function for new soundtrack

--- sound.py
+++ sound.py
@@ -2,10 +2,15 @@ import pygame
 
 def soundbackground():
     pygame.mixer.init()
-    sound = pygame.mixer.Sound("sounds\Title (Wii Play).mp3")
+    sound = pygame.mixer.Sound("sounds/Title (Wii Play).mp3")
     sound.play(loops=-1)
 
 def soundeffect():
     pygame.mixer.init()
     sound = pygame.mixer.Sound("sounds/chess hitting wood.mp3")
     sound.play()
+
+def soundbackground_tod():
+    pygame.mixer.init()
+    sound = pygame.mixer.Sound("sounds/The_Winner_Is_Soundtrack.mp3")
+    sound.play(loops=-1)
