d50f583f601c0157b9d3960eeb1315244092881b
Tod1805 <theodore.desmet18@gmail.com>
2026-04-19 23:41:39 +0200

    Refactor sound functions and adjust volume settings
    Adjusted sound effect volume and modified sound file paths.

--- sound.py
+++ sound.py
@@ -1,21 +1,16 @@
 import pygame
-
-def soundbackground():
-    pygame.mixer.init()
-    sound = pygame.mixer.Sound("sounds\Title (Wii Play).mp3")
-    sound.play(loops=-1)
-
 def soundeffect():
     pygame.mixer.init()
     sound = pygame.mixer.Sound("sounds/chess hitting wood.mp3")
+    sound.set_volume(0.1) # Réduire le volume de l'effet sonore       
     sound.play()
 
-def soundbackground_tod():
+def soundbackground():
     pygame.mixer.init()
     sound = pygame.mixer.Sound("sounds/The_Winner_Is_Soundtrack.mp3")
     sound.play(loops=-1)
 
 def vibration():
     pygame.mixer.init()
-    sound = pygame.mixer.Sound("sounds/vibration.ogg")
-    sound.play()
+    sound = pygame.mixer.Sound("sounds/vibration.mp3")
+    sound.play(maxtime=500) # Joue le son de vibration pendant 500 millisecondes pour simuler une vibration courte
